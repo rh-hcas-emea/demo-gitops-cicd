e1737d7be86333ed38f53321e41a6f6c09a6e42c
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2022-03-18 11:30:00 +0000

    add ci pipeline for api]

--- bin/github-webhook.sh
+++ bin/github-webhook.sh
@@ -8,9 +8,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROUTE='webhooks.examples-cicd.apps.paas.lab.stocky37.dev'
 TEMPLATE_FILE="$SCRIPT_DIR/templates/github.json"
 
-export NAME=greeting-ui
+#export NAME=greeting-ui
+export NAME=greeting-api
 export BRANCH=main
-export COMMIT=3af181b11e626881607a058861df6a675ba6f597
+#export COMMIT=3af181b11e626881607a058861df6a675ba6f597
+export COMMIT=873c59793302c8773af49aaf1adfb3146aceceea
 export COMMIT_MSG="Mock commit message"
 
 secret="${1:-secret}"; shift
